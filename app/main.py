5409ada8d7f281958520ee1dad47e2a07f7afd62
nogawanogawa <t_tsuno_tsuno@yahoo.co.jp>
2020-03-10 22:21:19 +0900

    [add] PATH パラメータ, Query パラメータ

--- app/main.py
+++ app/main.py
@@ -1,8 +1,21 @@
 from fastapi import FastAPI
 
-api = FastAPI()
+app = FastAPI()
+
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
 
 
-@api.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+@app.get("/items/{item_id}")
+async def read_item(item_id):
+    return {"item_id": item_id}
+
+
+@app.get("/query/")
+async def query_parameter(skip: int):
+    return {"skip": skip}
